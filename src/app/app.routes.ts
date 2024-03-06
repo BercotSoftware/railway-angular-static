f032fbb9771e00d369b0429289a8e8bfa027e3cf
Doug <doug.bercot@gmail.com>
2024-03-05 21:25:37 -0500

    Merging in pieces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import {CoursesComponent} from "./courses/courses.component";
+import {GroupsComponent} from "./groups/groups.component";
+import {CalendarComponent} from "./calendar/calendar.component";
+import {EventsComponent} from "./events/events.component";
+import {ContactsComponent} from "./contacts/contacts.component";
 
 export const routes: Routes = [
-  { path: "courses", component: CoursesComponent }
+  { path: "courses", component: CoursesComponent },
+  { path: "contacts", component: ContactsComponent },
+  { path: "groups", component: GroupsComponent },
+  { path: "calendar", component: CalendarComponent },
+  { path: "events", component: EventsComponent },
+  { path: "courses", component: CoursesComponent },
 ];
