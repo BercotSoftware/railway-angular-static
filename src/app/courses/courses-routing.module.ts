f1699f72ca0e5050106352035591a40ac895d9d2
Doug <doug.bercot@gmail.com>
2024-03-05 22:13:57 -0500

    More routing, skeletons

---
+++ src/app/courses/courses-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CoursesListComponent} from "./courses-list/courses-list.component";
+import {CoursesCreateComponent} from "./courses-create/courses-create.component";
+import {CoursesEditComponent} from "./courses-edit/courses-edit.component";
+import {CoursesViewComponent} from "./courses-view/courses-view.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: CoursesListComponent },
+  { path: 'create', component: CoursesCreateComponent },
+  { path: ':id/edit', component: CoursesEditComponent },
+  { path: ':id', component: CoursesViewComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+})
+export class CoursesRoutingModule { }
