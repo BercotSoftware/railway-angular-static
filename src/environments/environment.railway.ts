52a49a7d0138609725252ae168df2447b8fad98b
doug <doug.bercot@gmail.com>
2024-03-20 16:17:49 -0400

    Seems to work

--- src/environments/environment.railway.ts
+++ src/environments/environment.railway.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  PLAY_GOLF_API_URL: "http://localhost:4000",
+  PLAY_GOLF_API_URL: "https://nunjobiznezz.com/api",
   JWT_ISSUER_URL: "http://localhost:8100",
   JWT_ISSUER_CLIENT_ID: "play-golf",
 };
