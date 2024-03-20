3a89c52bfa5531f16f0fba9446ea2402f0f59ab3
doug <doug.bercot@gmail.com>
2024-03-20 09:05:19 -0400

    Kinda sorta almost working (interceptor)

--- src/environments/environment.railway.ts
+++ src/environments/environment.railway.ts
@@ -1,6 +1,10 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:4000",
-  authUrl: "http://localhost:8800",
-  defaultValue: "Woot!",
+  apiUrl: 'https://play-golf-server-production.up.railway.app',                      // The resource server
+  authUrl: 'https://keycloak-production-d6f9.up.railway.app/realms/play-golf',    // The authentication server
+  authClientId: "play-golf",
 };
