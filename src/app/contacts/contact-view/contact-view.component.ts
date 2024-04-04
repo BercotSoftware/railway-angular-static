600614ae0f05a0243b89801ebe67ca6045daf87c
Doug <doug.bercot@gmail.com>
2024-04-04 10:04:22 -0400

    Starting on views

--- src/app/contacts/contact-view/contact-view.component.ts
+++ src/app/contacts/contact-view/contact-view.component.ts
@@ -1,5 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {LoadingService} from "../../_services/loading.service";
+import {Contact, ContactsService} from "@golf-api";
+import {NotificationService} from "../../_services/notification.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-contact-view',
@@ -8,6 +12,43 @@ import {CommonModule} from '@angular/common';
   templateUrl: './contact-view.component.html',
   styleUrl: './contact-view.component.css'
 })
-export class ContactViewComponent {
+export class ContactViewComponent implements OnInit {
 
+  contact: Contact
+
+  constructor(
+    private loadingService: LoadingService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute,
+    private router : Router,
+    private contactsService: ContactsService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const contactId = this.route.snapshot.params['id']
+    if (contactId) {
+      this.fetchContact(contactId);
+    } else {
+      this.notificationService.error("Error loading contact")
+    }
+  }
+
+
+  fetchContact(id: string) {
+    this.loadingService.next(true)
+    this.contactsService.findContactById(id).subscribe({
+      next: (result) => {
+        this.contact = result
+        this.loadingService.next(false)
+      },
+      error: (err) => {
+        this.notificationService.error(err.error.message ?? "Error loading contact")
+        this.loadingService.next(false)
+      },
+      complete: () => {
+        this.loadingService.next(false)
+      }
+    })
+  }
 }
