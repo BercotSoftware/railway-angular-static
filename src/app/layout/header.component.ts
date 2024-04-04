58fce01294a4db98f3f84e2556b95f18e902e1f8
Doug <doug.bercot@gmail.com>
2024-04-04 08:45:22 -0400

    Popper - dropdowns

--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -49,6 +49,17 @@ import {OidcSecurityService} from "angular-auth-oidc-client";
             <li class="nav-item">
               <a class="nav-link" (click)="logOut()">Log Out</a>
             </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
           </ng-template>
 
 
