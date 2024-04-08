d760e79e63d48d0f0d0bf3f144b03069640d6f0a
Doug <doug.bercot@gmail.com>
2024-04-08 08:31:06 -0400

    Auth

--- src/app/auth/auth-config.ts
+++ src/app/auth/auth-config.ts
@@ -18,3 +18,23 @@ export const openIdConfiguration: OpenIdConfiguration = {
   secureRoutes: [environment.PLAY_GOLF_API_URL],
   logLevel: LogLevel.Debug,
 };
+
+export const googleOauthConfiguration : OpenIdConfiguration = {
+  authority: 'https://accounts.google.com',
+  redirectUrl: window.location.origin,
+  clientId: '188968487735-b1hh7k87nkkh6vv84548sinju2kpr7gn.apps.googleusercontent.com',
+  responseType: 'id_token token',
+  scope: 'openid email profile',
+  triggerAuthorizationResultEvent: true,
+  postLogoutRedirectUri: window.location.origin + '/unauthorized',
+  startCheckSession: false,
+  silentRenew: false,
+  silentRenewUrl: window.location.origin + '/silent-renew.html',
+  postLoginRoute: '/home',
+  forbiddenRoute: '/forbidden',
+  unauthorizedRoute: '/unauthorized',
+  logLevel: LogLevel.Debug,
+  historyCleanupOff: true,
+  // iss_validation_off: false
+  // disable_iat_offset_validation: true
+}
